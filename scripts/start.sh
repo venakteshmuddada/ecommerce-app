fe0921fccccd58a9c2c7b6a0d3c12b2efc29a481
venkateshmuddada <venkatesh.muddada2506@gmail.com>
2025-05-01 20:18:27 -0400

    Fix deployment scripts

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd /home/ec2-user/ecommerce-app/backend
-nohup node index.js > /home/ec2-user/backend.log 2>&1 &
+export AWS_REGION=us-east-1
+nohup node index.js > ~/backend.log 2>&1 &
