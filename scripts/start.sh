982e23b90c5984379a3111361ee1f51e4f4bd692
venakteshmuddada <venkatesh.muddada2506@gmail.com>
2025-05-01 20:36:57 -0400

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-cd /home/ec2-user/ecommerce-app/backend
+pkill node || true
+rm -rf /home/ec2-user/app
+mv /home/ec2-user/deploy_temp /home/ec2-user/app
+cd /home/ec2-user/app/backend
 export AWS_REGION=us-east-1
 nohup node index.js > ~/backend.log 2>&1 &
